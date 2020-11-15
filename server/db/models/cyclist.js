3cd56d96e34828a268c21c4f90df199161e69f22
tracilevine <televine@gmail.com>
2020-11-14 20:39:59 -0500

    creating basic models

--- server/db/models/cyclist.js
+++ server/db/models/cyclist.js
@@ -2,69 +2,92 @@ const mongoose = require('mongoose'),
   validator = require('validator'),
   bcrypt = require('jsonwebtoken');
 
-const cyclistSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-      trim: true,
-      lowercase: true,
-      validate(value) {
-        if (!validator.isEail(value)) {
-          throw new Error('Email is invalid.');
-        }
+const cyclistSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Invalid e-mail address.');
       }
-    },
-    password: {
-      type: String,
-      required: true,
-      trim: true,
-      validate(value) {
-        if (value.toLowerCase().includes('password')) {
-          throw new Error("Password can't be password.");
-        }
-        if (value.lenght < 8) {
-          throw new Error('Password must be at least 6 characters long.');
-        }
-      }
-    },
-    admin: {
-      type: Boolean,
-      required: true,
-      default: false
-    },
-    tokens: [
-      {
-        token: {
-          type: String,
-          required: true
-        }
-      }
-    ],
-    avatar: {
-      type: String
     }
   },
-  {
-    timestamps: true
-  }
-);
-
-//Creating relation between Cyclist and order
-
-cyclistSchema.virtual('task', {
-  ref: 'Task',
-  localField: '_id',
-  foreignField: 'owner'
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error("'Password' can't be used as a password.");
+      }
+      if (value.length < 6) {
+        throw new Error('Password must be at least 6 characters in length.');
+      }
+    }
+  },
+  zipcode: {
+    type: String,
+    trim: true
+  },
+  bicycles: [
+    {
+      bike: {
+        bikeName: {
+          type: String,
+          trim: true
+        },
+        bikeModel: {
+          type: String,
+          trim: true
+        },
+        bikeType: {
+          type: String,
+          trim: true
+        }
+      }
+    }
+  ],
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ],
+  avatar: {
+    type: String
+  },
+  phone: {
+    type: String,
+    trim: true
+  },
+  timestamps: true
 });
 
-cyclistSchema.methods.toJson = function () {
+//Creating relation between Cyclist and order
+cyclistSchema.virtual('cyclist', {
+  ref: 'ServiceOrder',
+  localField: '_id',
+  foreignField: 'cyclist'
+});
+
+//Creating relationship between Cyclist and Review
+cyclistSchema.virtual('review', {
+  ref: 'Review',
+  localField: '_id',
+  foreignField: 'reviewId'
+});
+
+cyclistSchema.methods.toJSON = function () {
   const cyclist = this;
   const cyclistObject = cylist.toObject;
   delete cyclistObject.password;
@@ -82,7 +105,6 @@ cyclistSchema.methods.generateAuthToken = async function () {
 
   user.tokens = user.tokens.concat({ token });
   await user.save();
-
   return token;
 };
 
@@ -95,22 +117,12 @@ cyclistSchema.statics.findByCredentials = async (email, password) => {
   return user;
 };
 
-// Mongoose Middleware to Hass users Passwords
+// Mongoose Middleware to Hash users Passwords
 cyclistSchema.pre('save', async function (next) {
-  const user = this;
+  const cyclist = this;
   if (cyclist.isModified('password'))
     cylist.password = await bcrypt.hash(cyclist.password, 8);
 
-  next();
-});
-
-// Delete user tasks when user is removed/
-
-cyclistSchema.pre('remove', async function (next) {
-  const user = this;
-  await Order.deleteMany({
-    owner: cyclist._id
-  });
   next();
 });
 
---
+++ server/db/models/reviews.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Reviews = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      trim: true
+    },
+    rating: {
+      type: Number,
+      trim: true
+    },
+    bikeshop: {
+      type: Schema.Types.ObjectId,
+      ref: 'Bikeshop'
+    },
+    cyclist: {
+      type: Schema.Types.ObjectId,
+      ref: 'cyclist'
+    }
+  },
+  { timestamps: true }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
