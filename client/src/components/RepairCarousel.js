4910b24dcdc344155b2fe4aee45a3ee3109a6271
DNiev <danny.nieves411@gmail.com>
2020-11-17 16:30:17 -0500

    pushing up changes to shoplist

--- client/src/components/RepairCarousel.js
+++ client/src/components/RepairCarousel.js
@@ -2,7 +2,11 @@ import React from 'react';
 import {} from '@material-ui/core';
 
 const RepairCarousel = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>This is the repair image carousel</h1>
+    </div>
+  );
 };
 
 export default RepairCarousel;
