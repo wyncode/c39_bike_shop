8e5aa2ec03be7ad318a8a2ab7b1cf5feda5941b8
JP Castrillon <jpcas001@gmail.com>
2020-11-18 18:50:56 -0500

    updating HowitWorks component

--- client/src/components/HowItWorks.js
+++ client/src/components/HowItWorks.js
@@ -3,8 +3,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const HowItWorks = () => {
   return (
-    <div>
-      <div class="card-deck"></div>
+    <div class="card-deck">
+      <div class="card">
+        <img src="..." class="card-img-top" alt="..."></img>
+        <div class="card-body">
+          <h5 class="card-title">Step 1: Select Your Bike Shop</h5>
+        </div>
+      </div>
+
+      <div class="card">
+        <img src="..." class="card-img-top" alt="..."></img>
+        <div class="card-body">
+          <h5 class="card-title">Step 2: Select Your Repairs</h5>
+        </div>
+      </div>
+      <div class="card">
+        <img src="..." class="card-img-top" alt="..."></img>
+        <div class="card-body">
+          <h5 class="card-title">Step 3: Make an appointment</h5>
+        </div>
+      </div>
+      <div class="card">
+        <img src="..." class="card-img-top" alt="..."></img>
+        <div class="card-body">
+          <h5 class="card-title">Step 4: Track Your Bike's Repair Progress!</h5>
+        </div>
+      </div>
     </div>
   );
 };
