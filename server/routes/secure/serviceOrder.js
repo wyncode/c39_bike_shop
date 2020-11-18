d3963686dda3bc3f0bde5b39e6f3fd3aede15838
tracilevine <televine@gmail.com>
2020-11-18 01:35:56 -0500

    creating routes for orders

--- server/routes/secure/serviceOrder.js
+++ server/routes/secure/serviceOrder.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
 
-const {} = require('../../controllers/serviceOrder');
+const {
+  createServiceOrder,
+  updateServiceOrder,
+  getSpecificOrder,
+  deleteServiceOrder
+} = require('../../controllers/serviceOrder');
+
+router.post('/', createServiceOrder);
+router.patch('/:id', updateServiceOrder);
+router.get('/:id', getSpecificOrder);
+router.delete('/:id', deleteServiceOrder);
