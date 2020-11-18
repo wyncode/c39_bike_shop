db476e2b4b00fed02bef68e4129d7164345cce82
tracilevine <televine@gmail.com>
2020-11-18 14:35:49 -0500

    repair pop up component

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { AppContextProvider } from './context/AppContext';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
-import ShopList from './pages/ShopList';
+import ShopList from './pages/Shoplist';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-
+import RepairPopUp from './components/RepairPopUp';
 import './App.css';
 
 const App = () => {
