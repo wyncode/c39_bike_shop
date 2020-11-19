49b1364c199bf9511d71aa4c40167b2bc4e0ed4b
JP Castrillon <jpcas001@gmail.com>
2020-11-18 19:46:42 -0500

    troubleshooting

--- client/src/components/FeaturedShops.js
+++ client/src/components/FeaturedShops.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function FeaturedShops() {
-  return <h1>FeaturedShops</h1>;
+  return (
+    <div id="bikeshops" class="carousel slide" data-rides="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="..." class="d-block w-100" alt="..."></img>
+        </div>
+      </div>
+    </div>
+  );
 }
