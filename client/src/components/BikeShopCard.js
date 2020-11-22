c266766c74c45e51940226943923040af1f98b95
JP Castrillon <jpcas001@gmail.com>
2020-11-21 22:01:59 -0500

    updatedbikeshopcard

--- client/src/components/BikeShopCard.js
+++ client/src/components/BikeShopCard.js
@@ -5,21 +5,23 @@ import '../App.css';
 const BikeShopCard = () => {
   return (
     <div>
-      <Container className="shopcard">
-        <Card>
-          <Container>
-            <Card.Img
-              src="https://i.imgur.com/f9TIgMc.png"
-              alt="Bike Shop Logo"
-              className="bikecard"
-            />
-          </Container>
-          <Container>
-            <Card.Title>Bike Shop Name</Card.Title>
-            <Card.Text>Contact Information</Card.Text>
-          </Container>
+      <Card>
+        <Container>
+          <Card.Img
+            className="shopimg1"
+            src="https://i.imgur.com/50JZigU.png"
+            alt="Bike Shop Logo"
+          />
+        </Container>
+        <Card className="text-center">
+          <Card.Body>
+            <Card.Title>City Bikes</Card.Title>
+            <Card.Text>888 Bike Chain Road, Miami, Florida, 33137</Card.Text>
+            <Card.Text>305-305-3053</Card.Text>
+            <Card.Text>Reviews</Card.Text>
+          </Card.Body>
         </Card>
-      </Container>
+      </Card>
     </div>
   );
 };
