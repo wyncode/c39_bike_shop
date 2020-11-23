088e7b97edea78a0b8d90fca585f9e7fcd1d4ae8
tommy <tommy.suwunrut@gmail.com>
2020-11-22 20:00:28 -0500

    reset server files

---
+++ server/app.js
@@ -0,0 +1,57 @@
+//import the DB connection
+require('./db/config/index');
+const express = require('express'),
+  app = express(),
+  openRoutes = require('./routes/open/index'),
+  userRouter = require('./routes/secure/user'),
+  repairRouter = require('./routes/secure/repair'),
+  cyclistRouter = require('./routes/secure/cyclist'),
+  bikeshopRouter = require('./routes/secure/bikeshop'),
+  reviewRouter = require('./routes/secure/reviews'),
+  orderRouter = require('./routes/secure/serviceorder'),
+  passport = require('./middleware/authentication/index'),
+  fileUpload = require('express-fileupload'),
+  cookieParser = require('cookie-parser'),
+  path = require('path');
+
+// Parse incoming JSON into objects
+app.use(express.json());
+
+//Unauthenticated routes
+app.use('/api', openRoutes);
+
+//Middleware to parse through incoming cookies in the requests.
+app.use(cookieParser());
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
+}
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/images'
+  })
+);
+
+//Authenticated Routes
+app.use('/api/*', passport.authenticate('jwt', { session: false }));
+
+app.use('/api/user', userRouter);
+app.use('/api/cyclist', cyclistRouter);
+app.use('/api/bikeshop', bikeshopRouter);
+app.use('/api/repair', repairRouter);
+app.use('/api/review', reviewRouter);
+app.use('/api/order', orderRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  // Handle React routing, return all requests to React app
+  app.get('*', (request, response) => {
+    response.sendFile(
+      path.resolve(__dirname, '..', 'client', 'build', 'index.html')
+    );
+  });
+}
+
+module.exports = app;
---
+++ server/controllers/serviceOrder.js
@@ -0,0 +1,75 @@
+const ServiceOrder = require('../db/models/serviceOrder');
+const Bikeshop = require('../db/models/bikeshop');
+
+exports.createOrder = async (req, res) => {
+  const newOrder = new ServiceOrder(req.body);
+  newOrder.bikeshop = req.body.bikeshop;
+  newOrder.cyclist = req.user._id;
+
+  try {
+    const bikeshop = await Bikeshop.findById(req.body.bikeshop);
+
+    const order = new ServiceOrder({
+      ...req.body
+    });
+    await order.save();
+
+    await bikeshop.orders.push(order);
+    await bikeshop.save();
+
+    res.status(201).json(order);
+  } catch (e) {
+    res.status(400).json({ error: e.toString() });
+  }
+};
+
+exports.getAllOrders = (req, res) => {
+  ServiceOrder.find()
+    .then((orders) => res.status(200).json(orders))
+    .catch((err) => res.status(500).json('Error: ' + err));
+};
+
+exports.updateOrder = async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['progress', 'dropoffDate', 'expectedPickup'];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation)
+    return res.status(400).json({ message: 'invalid updates' });
+  const filter = req.params.id;
+  try {
+    const order = await ServiceOrder.findByIdAndUpdate(filter);
+    if (!order) return res.status(404).json({ message: 'order not found' });
+    updates.forEach((update) => (order[update] = req.body[update]));
+    await order.save();
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.deleteOrderById = (req, res) => {
+  ServiceOrder.findByIdAndDelete(req.params.id)
+    .then((order) => {
+      if (!order) {
+        return res.status(404).json('Error: Order not found!');
+      }
+      res.status(204).json(order);
+    })
+    .catch((err) => {
+      res.status(500).json('Error: ' + err);
+    });
+};
+
+exports.getOrderById = async (req, res) => {
+  try {
+    const order = ServiceOrder.findById(req.params.id);
+    if (!order) {
+      return res.status(404).json('Error: Order not found!');
+    }
+    await res.status(200).json(order);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
