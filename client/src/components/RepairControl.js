9f8db3bb97dd0f2c7c6ff31a42ac592e1801d582
JP Castrillon <jpcas001@gmail.com>
2020-11-24 15:56:27 -0500

    updates to the control page

--- client/src/components/RepairControl.js
+++ client/src/components/RepairControl.js
@@ -1,25 +1,12 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
-import './styles/repairs.css';
+// import React from 'react';
+// import { Card } from 'react-bootstrap';
+// import './styles/repairs.css';
 
-const RepairControl = () => {
-  return (
-    <Card>
-      <Card.Header as="h5" class="d-flex justify-content-center">
-        Bike Repair Controls
-      </Card.Header>
-      <Card.Body>
-        <Card.Title class="d-flex justify-content-center">
-          Traci #55555
-        </Card.Title>
-        <Card.Text class="d-flex justify-content-center">
-          8-bit gluten-free roof party, drinking vinegar pickled aesthetic.
-          Microdosing fixie margarita fashion axe meh live-edge drinking
-          vinegar.
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  );
-};
+// const RepairControl = () => {
+//   return (
 
-export default RepairControl;
+//   );
+
+// }
+
+// export default RepairControl;
--- client/src/pages/BikeControl.js
+++ client/src/pages/BikeControl.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import RepairControl from '../components/RepairControl';
+import { Card, Container } from 'react-bootstrap';
+// import RepairControl from '../components/RepairControl';
+import Orders from '../components/Orders';
 
 import '../components/styles/ownerorderlist.css';
 
 const BikeControl = () => {
   return (
     <>
-      <RepairControl />
+      <h1 class="d-flex justify-content-center">Bike Repair Controls</h1>
+      {/* <RepairControl /> */}
+      <h5>Repair Description</h5>
+      <p>
+        8-bit gluten-free roof party, drinking vinegar pickled aesthetic.
+        Microdosing fixie margarita fashion axe meh live-edge drinking vinegar.
+      </p>
+      <Container>
+        <Card.Img
+          className="bikepic"
+          src="https://i.imgur.com/ty0FCH0.png"
+        ></Card.Img>
+      </Container>
+
+      <Orders />
     </>
   );
 };
