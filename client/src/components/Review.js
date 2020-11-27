b76577a5d69349f87c1ac15f24271bdd2fe2fcaf
tracilevine <televine@gmail.com>
2020-11-27 00:07:35 -0500

    add review feature

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import './styles/subpages.css';
+import Rater from 'react-rater';
+import 'react-rater/lib/react-rater.css';
+import { Card } from 'react-bootstrap';
 
 const Review = ({ review }) => {
   return (
     <div>
       <div className="reviewsHeader"></div>
-      <ul>
-        {review?.map((review) => (
-          <li key={review._id} className="list">
-            <div>
-              <p className="stars">{review.rating}</p>
-              <p>{review.review}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+
+      {review?.map((review) => (
+        <Card key={review._id} className="list">
+          <Rater rating={review.rating} interactive={false} color={'#00fff9'} />
+          <p>{review.review}</p>
+        </Card>
+      ))}
     </div>
   );
 };
--- client/src/pages/BikeShopPDP.js
+++ client/src/pages/BikeShopPDP.js
@@ -1,15 +1,17 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
-import { Container, Button, Card } from 'react-bootstrap';
+import { Container, Button, Card, Form, Modal } from 'react-bootstrap';
 import Map from '../components/Map';
 import BikeShopCard from '../components/BikeShopCard';
 import Review from '../components/Review';
 import Footer from '../components/Footer';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import swal from 'sweetalert';
 
 const BikeShopPDP = ({ match, history }) => {
-  const { setBikeshop, bikeshop } = useContext(AppContext);
+  const { setBikeshop, bikeshop, user } = useContext(AppContext);
+  const [reviewData, setReviewData] = useState(null);
   const id = match.params.id;
 
   useEffect(() => {
@@ -20,15 +22,76 @@ const BikeShopPDP = ({ match, history }) => {
       });
   }, []);
 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleChange = (e) => {
+    setReviewData({ ...reviewData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    const form = e.target;
+    e.preventDefault();
+    try {
+      await axios({
+        method: 'POST',
+        url: `/api/review/${id}/review`,
+        withCredentials: true,
+        data: reviewData,
+        bikeshop: bikeshop._id,
+        cyclist: user._id
+      });
+      swal('New Review!', 'You review has been added!', 'success');
+      setReviewData(null);
+    } catch (error) {
+      swal('Oops!', 'Something went wrong');
+    }
+  };
+
   return (
     <>
       <Container className="d-flex flex-column align-items-center justify-content-center">
         <Map />
         <BikeShopCard bikeshop={bikeshop} />
         <hr className="pink-line-page-break" />
-        <Button onSubmit={() => history.push(`/review/${bikeshop?._id}`)}>
-          Add a Review
-        </Button>
+        <Button onSubmit={handleShow}>Add a Review</Button>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Dialog>
+            <Modal.Header closeButton>
+              <Modal.Title>Add A Review</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Form.Label>Give this bikeshop a Rating from 1-5</Form.Label>
+                <Form.Control
+                  name="rating"
+                  type="number"
+                  id="rating"
+                  max="5"
+                  onChange={handleChange}
+                />
+                <Form.Label>Write your Review of the Bikeshop</Form.Label>
+                <Form.Control
+                  name="review"
+                  id="review"
+                  type="type-area"
+                  onChange={handleChange}
+                />
+              </Form>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={handleSubmit}>
+                Save changes
+              </Button>
+            </Modal.Footer>
+          </Modal.Dialog>
+        </Modal>
         <Card className="review">
           {bikeshop &&
             bikeshop.reviews?.map((review) => (
