29665b9bcf58b5ed565d31310fe5869b60ae8323
tracilevine <televine@gmail.com>
2020-11-26 19:59:30 -0500

    added code

--- client/src/components/Review.js
+++ client/src/components/Review.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
-const Review = () => {
-  return <div>This is the review page</div>;
+const Review = ({ reviews }) => {
+  const total = review.length;
+
+  return (
+    <div>
+      <div className="reviewsHeader">
+        <div className="stars">
+          <img src={require('../images/stars.png')} alt="reviews" />
+          {total === 1 ? `${total} Review` : `${total} Reviews`}
+        </div>
+        <button className="button"> + Review</button>
+      </div>
+      <ul>
+        {reviews?.map((review) => (
+          <li key={review._id} className="list">
+            <div>
+              <p className="stars">
+                <img src={'#'} alt="reviews" />
+                {review.rating}
+              </p>
+              <p>{review.review}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Review;
