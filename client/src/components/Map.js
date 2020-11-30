dfce9a38ff44bcd66bfaebfb345b7e2ab1bf576c
tracilevine <televine@gmail.com>
2020-11-30 10:43:31 -0500

    git pull

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import ReactMapboxGl, { Marker, ZoomControl } from 'react-mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import ReactMapboxGl, { ZoomControl, Marker } from 'react-mapbox-gl';
 import '../components/styles/shoplist.css';
 
 const MapBox = ReactMapboxGl({
@@ -18,7 +18,7 @@ const Map = () => {
       center={[-80.1918, 25.7617]}
     >
       <Marker coordinates={[-80.1918, 25.7617]} className="marker">
-        <img className="marker-icon" />
+        <img className="marker-icon" src={bottle} />
       </Marker>
       <ZoomControl />
     </MapBox>
